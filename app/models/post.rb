5ed062a6c3042b3f3ad17a59d4b5b839ede059d1
martynj2001 <martynj2010@icloud.com>
2019-05-03 19:27:38 +0100

    gflBlog: Restart commit

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ApplicationRecord
+
+    belongs_to :category
+    validates :title, :content, :category_id, presence: true
+    has_many :line_items, inverse_of: :order
+
+    def index
+
+        if params[:category].blank?
+            @posts = Post.all.order("created_at DESC")
+        else
+            @category_id = Category.find_by(name: params[:category]).id
+            @posts = Post.where(category_id: @category_id).order("cretaed_at DESC")
+        end
+    end
+end
